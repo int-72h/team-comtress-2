a0e52c1efac804af094e199b1c43d7d6fa226f50
melvyn2 <melvyn.depeyrot@gmail.com>
2020-09-24 13:56:01 -1000

    add TF2 existance check

--- link.sh
+++ link.sh
@@ -6,6 +6,13 @@ steam="$HOME/.steam/steam"
 
 live_install="$steam/steamapps/common/Team Fortress 2"
 
+if [ ! -d "$live_install" ]; then
+	echo Failed to find TF2 at \'$live_install\'
+	echo Did you move your steam library?
+	echo If so, change the \`steam\` variable in \`link.sh\`
+	exit 1
+fi
+
 ln -s "$live_install/hl2" 		"../game/hl2"
 ln -s "$live_install/platform"	"../game/platform"
 ln -s "$live_install/tf/maps"	"../game/tf/maps"
