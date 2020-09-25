dd454c23171bbfad778f9b1083c8b410b6865ae5
HurricanePootis <eatabakhsh04@protonmail.com>
2020-09-25 10:44:14 -0500

    Updated link.sh so that you can put tf2 install path afterwards

--- link.sh
+++ link.sh
@@ -18,4 +18,11 @@ ln -s "$live_install/platform"	"../game/platform"
 ln -s "$live_install/tf/maps"	"../game/tf/maps"
 ln -s "$live_install/tf/"*.vpk	"../game/tf/"
 
+echo If you get \'cp: cannot overwrite non-directory\', that\'s normal
+
 cp -rn "$live_install/"*		"../game/"
+
+rm -r ../game/tf/custom
+
+cp game_clean/copy/bin/*.so ../game/bin/
+cp -r game_clean/copy/tf/custom/ ../game/tf/custom
