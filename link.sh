0ebb4bd4dc37524554bcb24410a7a004814b0341
melvyn2 <melvyn.depeyrot@gmail.com>
2020-09-25 08:52:57 -1000

    replace more copies with links in link.sh

--- link.sh
+++ link.sh
@@ -1,33 +1,42 @@
 #!/usr/bin/env sh
 cd "$(dirname "$0")" || exit
 
-# Edit this if installed elsewhere
-steam="$HOME/.steam/steam"
-
-
-if [ ! -f $steam/steamapps/Team\ Fortress\ 2/ ]; then
-    live_install=$1
+if [ -z "$1" ]; then
+    live_install="$HOME/.steam/steam/steamapps/common/Team Fortress 2/"
 else
-    live_install=$steam/steamapps/Team\ Fortress\ 2/
+	live_install="$1"
 fi
 
 if [ ! -d "$live_install" ]; then
-    echo Failed to find TF2 at \'$steam/steamapps/Team\ Fortress\ 2/\'
+    echo Failed to find TF2 at \'"$live_install"\'
     echo Did you move your steam library?
     echo If so, put the path to your TF2 folder after \'link.sh\'
     exit 1
 fi
 
-ln -s "$live_install/hl2" 		"../game/hl2"
-ln -s "$live_install/platform"	"../game/platform"
-ln -s "$live_install/tf/maps"	"../game/tf/maps"
-ln -s "$live_install/tf/"*.vpk	"../game/tf/"
+link_dir() {
+	ln -s "$live_install/$1" "../game/$1"
+}
 
-echo If you get \'cp: cannot overwrite non-directory\', that\'s normal
+link_glob() {
+	ln -s "$live_install/$1/"*".$2" "../game/$1/"
+}
 
-cp -rn "$live_install/"*		"../game/"
+link_dir hl2
+link_dir platform
+link_dir tf/maps
+mkdir ../game/tf/materials
+link_dir tf/materials/models
+link_dir tf/materials/vgui
+link_dir tf/media
+link_dir tf/resource
+link_dir tf/scripts
 
-rm -r ../game/tf/custom
+link_glob bin 'so'
+link_glob tf 'vpk'
+
+cp -r "$live_install/tf/cfg" "../game/tf/cfg"
+cp "$live_install/tf/gameinfo.txt" "../game/tf/"
 
 cp game_clean/copy/bin/*.so ../game/bin/
 cp -r game_clean/copy/tf/custom/ ../game/tf/custom
