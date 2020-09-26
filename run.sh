6d71e12087d4e19c98d2be7b8b269f8075b844c5
interrupt <int-10h@protonmail.com>
2020-09-26 19:59:19 +0000

    Tidied up mangohud integration

--- run.sh
+++ run.sh
@@ -2,11 +2,13 @@
 
 export LD_LIBRARY_PATH="/usr/lib32:./bin:./tf/bin:${LD_LIBRARY_PATH}"
 echo "LD_LIBRARY_PATH = ${LD_LIBRARY_PATH}"
+
 if [ -f "/usr/bin/mangohud" ]; then
   mango="mangohud" 
 fi
+
 if [ "$1" = '-d' ]; then
   cd ../game && lldb ./hl2_linux -- -steam -game tf -insecure -novid -nojoy -nosteamcontroller -nohltv -particles 1 -noborder -particle_fallback 2 -dev -allowdebug
 else
-  cd ../game && MANGOHUD_DLSYM=1 $mango ./hl2_linux -- -steam -game tf -insecure -novid -nojoy -nosteamcontroller -nohltv -particles 1 -noborder -particle_fallback 2 -dev -allowdebug
+  cd ../game && MANGOHUD_DLSYM=1 $mango ./hl2_linux -steam -game tf -insecure -novid -nojoy -nosteamcontroller -nohltv -particles 1 -noborder -particle_fallback 2 -dev -allowdebug
 fi
